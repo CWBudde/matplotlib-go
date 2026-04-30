d7dc11fe5afb064efbe0beb9e322e74e3180ecfd
Christian Budde <Christian.Budde@meko.de>
2026-05-01 00:58:44 +0200

    backends: regression tests pinning SaveFormats maps for agg and svg

---
+++ backends/svg/registry_test.go
@@ -0,0 +1,28 @@
+package svg
+
+import (
+	"testing"
+
+	"matplotlib-go/backends"
+)
+
+func TestSVGBackend_PopulatesSaveFormats(t *testing.T) {
+	info, ok := backends.DefaultRegistry.Get(backends.SVG)
+	if !ok {
+		t.Fatal("SVG backend not registered")
+	}
+	if info.SaveFormats == nil {
+		t.Fatal("SVG SaveFormats map is nil")
+	}
+	if _, ok := info.SaveFormats[".svg"]; !ok {
+		t.Fatalf("SVG SaveFormats missing .svg handler; got keys: %v", mapKeys(info.SaveFormats))
+	}
+}
+
+func mapKeys(m map[string]backends.SaveHandler) []string {
+	keys := make([]string, 0, len(m))
+	for k := range m {
+		keys = append(keys, k)
+	}
+	return keys
+}
